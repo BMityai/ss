12a40b4c9d8b7d09b7284c20c7930f9267d70ee5
Medet <b.m.bakytzhan@gmail.com>
2021-09-07 22:58:09 +0600

    block positions render

--- src/components/adminhtml/content/elements/blocks/EditForm.vue
+++ src/components/adminhtml/content/elements/blocks/EditForm.vue
@@ -51,7 +51,7 @@
 
                         <!-- block -->
                         <div class="field block">
-                            <div class="layout">Block</div>
+                            <div class="layout">Block Type</div>
                             <div class="value">
                                 <Dropdown
                                     :filter="true"
@@ -64,21 +64,62 @@
                             </div>
                         </div>
 
-                        <!-- block -->
+                        <!-- page type -->
                         <div class="field page_type">
                             <div class="layout">Page Type</div>
                             <div class="value">
                                 <Dropdown
                                     :filter="true"
-                                    v-model="form.value.blockId"
-                                    :options="contentBlockOptions.pageTypeOptions"
+                                    v-model="form.value.pageTypeId"
+                                    :options="
+                                        contentBlockOptions.pageTypeOptions
+                                    "
                                     optionLabel="value"
                                     optionValue="id"
-                                    placeholder="Select a block"
+                                    placeholder="Select a type"
+                                />
+                            </div>
+                        </div>
+
+                        <!-- position -->
+                        <div class="field page_type">
+                            <div class="layout">Position On Page</div>
+                            <div class="value">
+                                <Dropdown
+                                    :filter="true"
+                                    v-model="form.value.positionId"
+                                    :options="
+                                        contentBlockOptions.positionOptions[
+                                            form.value.pageTypeId
+                                        ]
+                                    "
+                                    optionLabel="value"
+                                    optionValue="id"
+                                    placeholder="Select a position"
                                 />
                             </div>
                         </div>
                     </div>
+                </AccordionTab>
+                <AccordionTab header="Block Items Configuration">
+                    <DataTable
+                        :value="form.value.items"
+                        :reorderableColumns="false"
+                        @rowReorder="onRowReorder"
+                        responsiveLayout="scroll"
+                    >
+                        <Column
+                            :rowReorder="true"
+                            headerStyle="width: 3rem"
+                            :reorderableColumn="false"
+                        />
+                        <Column
+                            v-for="col of columns"
+                            :field="col.field"
+                            :header="col.header"
+                            :key="col.field"
+                        >rrrr</Column>
+                    </DataTable>
                 </AccordionTab>
             </Accordion>
         </div>
@@ -93,6 +134,8 @@ import Accordion from "primevue/accordion";
 import AccordionTab from "primevue/accordiontab";
 import Dropdown from "primevue/dropdown";
 import SelectButton from "primevue/selectbutton";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 
 export default defineComponent({
     props: {
@@ -105,6 +148,8 @@ export default defineComponent({
         AccordionTab,
         Dropdown,
         SelectButton,
+        DataTable,
+        Column,
     },
 
     async setup(props) {
@@ -116,16 +161,31 @@ export default defineComponent({
         }
         const contentBlockOptions = await contentService.getBlockDictOptions();
 
-        console.log(contentBlockOptions)
+        const enableOptions = ref([
+            { label: "Enable", value: 1 },
+            { label: "Disable", value: 0 },
+        ]);
 
-       
-        const enableOptions = ref([{label:'Enable', value: 1}, {label:'Disable', value: 0}]);
+        console.log(form)
 
-        // Promise.allSettled()
+        const columns = ref([
+            {field: 'id', header: 'Id'},
+            {field: 'url', header: 'Url'},
+            {field: 'image', header: 'Image'},
+            {field: 'position', header: 'Position'}
+        ]);
+
+        const onRowReorder = (event) => {
+            console.log(event.value)  // @todo Установить значение position в зависимости порядка в массиве
+            form.value.items = event.value;
+        }
+
         return {
             form,
             contentBlockOptions,
-            enableOptions
+            enableOptions,
+            columns,
+            onRowReorder
         };
     },
 });
