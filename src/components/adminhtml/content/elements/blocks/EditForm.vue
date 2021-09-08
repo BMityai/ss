a6691f64f2cd8b371fa414eeac04d7da6f114f9c
Medet <b.m.bakytzhan@gmail.com>
2021-09-08 19:04:30 +0600

    block edit form

--- src/components/adminhtml/content/elements/blocks/EditForm.vue
+++ src/components/adminhtml/content/elements/blocks/EditForm.vue
@@ -102,24 +102,75 @@
                     </div>
                 </AccordionTab>
                 <AccordionTab header="Block Items Configuration">
+                   
                     <DataTable
                         :value="form.value.items"
                         :reorderableColumns="false"
                         @rowReorder="onRowReorder"
                         responsiveLayout="scroll"
                     >
+                        <Column :rowReorder="true"> </Column>
                         <Column
-                            :rowReorder="true"
-                            headerStyle="width: 3rem"
-                            :reorderableColumn="false"
-                        />
+                            field="url"
+                            header="Url"
+                            ref="url"
+                            headerStyle="width: 50%; text-align:center"
+                        >
+                            <template #body="{ data }">
+                                <InputText
+                                    type="text"
+                                    v-model="data.url"
+                                    style="width: 100%"
+                                />
+                            </template>
+                        </Column>
                         <Column
-                            v-for="col of columns"
-                            :field="col.field"
-                            :header="col.header"
-                            :key="col.field"
-                        >rrrr</Column>
+                            class="item_image"
+                            ref="image"
+                            field="image"
+                            header="Image"
+                        >
+                            <template #body="{ data }">
+                                <Image :src="data.image" alt="Image" preview v-if="data.image"/>
+                                <img class="p-image" src="@/assets/frontend/images/service/adminhtml/placeholder.png" v-if="!data.image || data.image === ''" alt="">
+                                <input
+                                    type="file"
+                                    hidden
+                                    ref="fileInput"
+                                    @change="onUpload"
+                                />
+                                <Button
+                                    icon="pi pi-upload"
+                                    label="Choose"
+                                    class="
+                                        select_image
+                                        p-button-raised p-button-text
+                                    "
+                                    @click="
+                                        fileInput.click();
+                                        selectItem(data);
+                                    "
+                                />
+                            </template>
+                        </Column>
+                        <Column>
+                            <template #body="{ data }">
+                                <Button
+                                    type="button"
+                                    icon="pi pi-trash"
+                                    :data-block-id="data.id"
+                                    @click="deteItem(data)"
+                                    class="p-button-danger"
+                                /> </template
+                        ></Column>
                     </DataTable>
+                    <div class="action_block">
+                        <Button
+                            label="Add Item"
+                            class="p-button-raised p-button-success"
+                            @click="addItem"
+                        />
+                    </div>
                 </AccordionTab>
             </Accordion>
         </div>
@@ -136,6 +187,8 @@ import Dropdown from "primevue/dropdown";
 import SelectButton from "primevue/selectbutton";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import Button from "primevue/button";
+import Image from "primevue/image";
 
 export default defineComponent({
     props: {
@@ -150,6 +203,8 @@ export default defineComponent({
         SelectButton,
         DataTable,
         Column,
+        Button,
+        Image,
     },
 
     async setup(props) {
@@ -166,18 +221,67 @@ export default defineComponent({
             { label: "Disable", value: 0 },
         ]);
 
-        console.log(form)
+        console.log(contentBlockOptions);
 
         const columns = ref([
-            {field: 'id', header: 'Id'},
-            {field: 'url', header: 'Url'},
-            {field: 'image', header: 'Image'},
-            {field: 'position', header: 'Position'}
+            { field: "url", header: "Url" },
+            { field: "image", header: "Image" },
         ]);
 
         const onRowReorder = (event) => {
-            console.log(event.value)  // @todo Установить значение position в зависимости порядка в массиве
+            console.log(event.value); // @todo Установить значение position в зависимости порядка в массиве
             form.value.items = event.value;
+        };
+
+        const onUpload = (event) => {
+            const file = event.target.files[0];
+            const allowedFormats = ["jpg", "jpeg", "png", "gif"];
+            const typeSplit = file["type"].split("/");
+            if (
+                typeSplit[0] !== "image" ||
+                !allowedFormats.includes(typeSplit[1])
+            ) {
+                fileInput.value = null;
+                return;
+            }
+
+            for (const item of form.value.items) {
+                if (item.id !== editedItemId.value) {
+                    continue;
+                }
+                console.log(item);
+                item.image = window.URL.createObjectURL(event.target.files[0]);
+            }
+        };
+
+        const editedItemId = ref();
+
+        const selectItem = (item) => {
+            console.log(item);
+            editedItemId.value = item.id;
+        };
+
+        const fileInput = ref();
+
+        const addItem = () => {
+            console.log({
+                id: "new-item-" + Date.now(),
+                image: "",
+                position: "",
+                url: "",
+            });
+            form.value.items.push({
+                id: "new-item-" + Date.now(),
+                image: "",
+                position: "",
+                url: "",
+            });
+        };
+
+        const deteItem = (item) => {
+            
+            const itemIndex = form.value.items.indexOf(item);
+            form.value.items.splice(itemIndex, 1);
         }
 
         return {
@@ -185,7 +289,12 @@ export default defineComponent({
             contentBlockOptions,
             enableOptions,
             columns,
-            onRowReorder
+            onRowReorder,
+            onUpload,
+            selectItem,
+            fileInput,
+            addItem,
+            deteItem
         };
     },
 });
